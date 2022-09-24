e898bdc968cdf371f19af6f639086091d51e772f
EffectiveSpace <acmarkov7@gmail.com>
2022-09-24 20:13:07 +0300

    new files

---
+++ game.py
@@ -0,0 +1,66 @@
+#Пример №2 крестики - нолики
+maps = [1,2,3,
+        4,5,6,
+        7,8,9]
+
+victories_line = [[0,1,2],
+                  [3,4,5],
+                  [6,7,8],
+                  [0,3,6],
+                  [1,4,7],
+                  [2,5,8],
+                  [0,4,8],
+                  [2,4,6]]
+
+def print_maps():
+    print(maps[0], end = " ")
+    print(maps[1], end = " ")
+    print(maps[2])
+    
+    print(maps[3], end = " ")
+    print(maps[4], end = " ")
+    print(maps[5])
+    
+    print(maps[6], end = " ")
+    print(maps[7], end = " ")
+    print(maps[8])
+    
+def step_maps(step, symbol):
+    ind = maps.index(step)
+    maps[ind] = symbol
+
+def get_result():
+    win = ""
+    
+    for i in victories_line:
+        if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
+            win = "X"
+        if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
+            win = "O"
+    return win
+
+game_over = False
+player_1 = True
+
+while game_over == False:
+    print_maps()
+    
+    if player_1 == True:
+        symbol = "X"
+        step = int(input('Человек 1, ваш ход: '))
+    else:
+        symbol = "O"
+        step = int(input('Человек 2, ваш ход: '))
+    
+    step_maps(step, symbol)
+    win = get_result()
+    
+    if win != "":
+        game_over = True
+    else:
+        game_over = False
+        
+    player_1 = not(player_1)
+    
+print_maps()
+print("Победил", win)
